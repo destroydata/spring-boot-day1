e8585b6b42995c226df5ab676794288061dc2810
tjdanqkr <tjdanqkr@gmail.com>
2023-07-11 15:16:36 +0900

    test

---
+++ src/main/java/com/example/demo/store/MemberHobby.java
@@ -0,0 +1,9 @@
+package com.example.demo.store;
+
+import com.example.demo.hobby.Hobby;
+import com.example.demo.member.Member;
+
+public class MemberHobby {
+    private Member member;
+    private Hobby hobby;
+}
