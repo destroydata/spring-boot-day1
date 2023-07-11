1515993056b4224bdd5ec3562e3f14b9acd2a05e
tjdanqkr <tjdanqkr@gmail.com>
2023-07-11 12:02:54 +0900

    ch

--- src/main/java/com/example/demo/member/MemberResponse.java
+++ src/main/java/com/example/demo/member/MemberResponse.java
@@ -1,6 +1,7 @@
 package com.example.demo.member;
 
 import com.example.demo.hobby.Hobby;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -11,19 +12,26 @@ public class MemberResponse {
     private Integer id;
     private String name;
     private Integer age;
-    private List<Hobby> hobbies = new ArrayList<>();
+    private List<HobbyDto> hobbies = new ArrayList<>();
     public MemberResponse(Member member){
         this.id = member.getId();
         this.age = member.getAge();
         this.name = member.getName();
-        List<Hobby> hobbies = member.getHobbies();
-        for (int i = 0; i < hobbies.size(); i++) {
-            Hobby hobby = new Hobby(hobbies.get(i).getId()
-                    , hobbies.get(i).getName()
-                    , null);
-            this.hobbies.add(hobby);
-        }
+//        List<Hobby> hobbies = member.getHobbies();
+//        for (int i = 0; i < hobbies.size(); i++) {
+//            HobbyDto hobby = new HobbyDto(hobbies.get(i).getId()
+//                    , hobbies.get(i).getName());
+//            this.hobbies.add(hobby);
+//        }
+        this.hobbies = member.getHobbies()
+                .stream()
+                .map(m -> new HobbyDto(m.getId(), m.getName()))
+                .toList();
 
     }
-
+    @Getter @AllArgsConstructor
+    class HobbyDto{
+        private Integer id;
+        private String name;
+    }
 }
